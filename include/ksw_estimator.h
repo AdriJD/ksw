951b9df34cdb8dae73c743348abc3820c00ad6f5
AdriJD <adri.j.duivenvoorden@gmail.com>
2021-02-13 21:56:20 -0500

    now plan ffts before parallel region to avoid critical region

--- include/ksw_estimator.h
+++ include/ksw_estimator.h
@@ -21,10 +21,45 @@
 
 float t_cubic_on_ring_sp(int *rule, float *f_i_phi, int nrule, int nphi);
 
+/*
+ * Convolve a single ring of the map with all unique bispectrum factors.
+ *
+ * Arguments
+ * ---------
+ * f_i_ell   : (nufact * npol * nell) array with unique factors.
+ * a_m_ell   : (npol * nell * nell) complex array with ell-major alms.
+ * y_m_ell   : (nell * nell) array with ell-major Ylms.
+ * m_ell_m   : (npol * nell * nm) complex array as input for ring fft, nm = nphi / 2 + 1.
+ * n_ell_phi : (npol * nell * nphi) array as output for ring fft.
+ * plan_c2r  : fftw plan for ring complex2real fft.
+ * f_i_phi   : (nufact * nphi) array for output unique factors on ring.
+ * nell      : Number of multipoles.
+ * npol      : Number of polarization dimensions.
+ * nufact    : Number of unique factors.
+ * nphi      : Number of phi per ring.
+ */
 
 void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		 float complex *m_ell_m, float *n_ell_phi, fftwf_plan plan_c2r,
 		 float *f_i_phi, int nell, int npol, int nufact, int nphi);
+
+/*
+ * Compute T[a] for a collection of rings.
+ *
+ * Arguments
+ * ---------
+ * ct_weights : (ntheta) array of quadruture weights for cos(theta).
+ * rule       : (nrule, 3) array
+ * f_i_ell    : (nufact * npol * nell) array with unique factors.
+ * a_m_ell    : (npol * nell * nell) complex array with ell-major alms.
+ * y_m_ell    : (ntheta * nell * nell) array with ell-major Ylms for each ring.
+ * ntheta     : number of thetas (rings).
+ * nrule      : number of rules.
+ * nell       : Number of multipoles.
+ * npol       : Number of polarization dimensions.
+ * nufact     : Number of unique factors.
+ * nphi       : Number of phi per ring.
+ */
 
 float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_m_ell, 
 		 double *y_m_ell, int ntheta, int nrule, int nell, int npol, int nufact,
--- src/estimator.c
+++ src/estimator.c
@@ -20,41 +20,8 @@ float t_cubic_on_ring_sp(int *rule, float *f_i_phi, int nrule, int nphi){
 	    t_cubic += f_i_phi[rx*nphi+pidx] * f_i_phi[ry*nphi+pidx] * f_i_phi[rz*nphi+pidx];
 	}
     }
-
     return t_cubic;
 }
-
-// backward for single theta.
-// input f_i_ell, m_ell_m, n_ell_phi, a_m_ell, y_m_ell
-// output f_i_phi
-
-// Openmpd over theta, so each thread gets unique slice into y_ell_m
-// Every thread needs private m_ell_m, f_i_ell, f_i_phi, n_ell_phi.
-// a_m_ell is shared and accessed by everyone.
-// So in function calling backward you need a parallel region where each
-// allocates  m_ell_m, f_i_ell, f_i_phi arrays, then start a parallel loop
-// imagine 5000 thetas, 200 threads : 25 passes through loop, that should be
-// enough to dwarf the cost of allocating memory.
-// If it's too slow, you can move the allocation outide the loop over step, like
-// in the python code. but it's ugly because you need to know the number of threads.
-
-/*
- * Convolve a single ring of the map with all unique bispectrum factors.
- *
- * Arguments
- * ---------
- * f_i_ell   : (nufact * npol * nell) array with unique factors.
- * a_m_ell   : (npol * nell * nell) complex array with ell-major alms.
- * y_m_ell   : (nell * nell) array with ell-major Ylms.
- * m_ell_m   : (npol * nell * nm) complex array as input for ring fft, nm = nphi / 2 + 1.
- * n_ell_phi : (npol * nell * nphi) array as output for ring fft.
- * plan_c2r  : fftw plan for ring complex2real fft.
- * f_i_phi   : (nufact * nphi) array for output unique factors on ring.
- * nell      : Number of multipoles.
- * npol      : Number of polarization dimensions.
- * nufact    : Number of unique factors.
- * nphi      : Number of phi per ring.
- */
 
 void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		 float complex *m_ell_m, float *n_ell_phi, fftwf_plan plan_c2r,
@@ -86,7 +53,7 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
     // or forward. Unlike numpy and pyfftw which apply 1/nphi during 
     // backward (c2r). So in python version I multiply the result of 
     // the fft by nphi to compensate for that factor. Here it's not needed.
-    fftwf_execute(plan_c2r);
+    fftwf_execute_dft_c2r(plan_c2r, m_ell_m, n_ell_phi);
 
     // f_i_ell @ n_ell_phi -> f_i_phi.
     cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
@@ -96,29 +63,24 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		0.0, f_i_phi, nphi);    
 }
 
-//    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-//		m, n, k, 1.0, mat_a, k, mat_b + tidx * k * n, n, 0.0, mat_c + tidx * m * n, n);
+void forward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
+		float complex *m_ell_m, float *n_ell_phi, fftwf_plan plan_r2c,
+		float *f_i_phi, float *x_i_ell, float *y_i_ell, float *z_i_ell,
+		float *dtdx_i_phi, float *dtdy_i_phi, float *dtdz_i_phi, int *rule,
+		float *weights, int nrule, int nell, int npol, int nufact, int nphi){
 
+    for (int ridx=0; ridx<nrule; ridx++){
 
-// needs all args to backward, t_cubic_on_ring, + theta, theta_weights. NOOO because you allocate a bunch.
+	int rx = rule[ridx*3];
+	int ry = rule[ridx*3+1];
+	int rz = rule[ridx*3+2];
 
-/*
- * Compute T[a] for a collection of rings.
- *
- * Arguments
- * ---------
- * ct_weights : (ntheta) array of quadruture weights for cos(theta).
- * rule       : (nrule, 3) array
- * f_i_ell    : (nufact * npol * nell) array with unique factors.
- * a_m_ell    : (npol * nell * nell) complex array with ell-major alms.
- * y_m_ell    : (ntheta * nell * nell) array with ell-major Ylms for each ring.
- * ntheta     : number of thetas (rings).
- * nrule      : number of rules.
- * nell       : Number of multipoles.
- * npol       : Number of polarization dimensions.
- * nufact     : Number of unique factors.
- * nphi       : Number of phi per ring.
- */
+	float wx = weights[ridx*3];
+	float wy = weights[ridx*3+1];
+	float wz = weights[ridx*3+2];	
+
+    }
+}
 
 float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_m_ell, 
 		 double *y_m_ell, int ntheta, int nrule, int nell, int npol, int nufact,
@@ -126,13 +88,27 @@ float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_
 
     int nm = nphi / 2 + 1;
     float t_cubic = 0.0;
+    int nffts[1] = {nphi};
+    fftwf_plan plan_c2r;
+
+    // Plan fft on temporary arrays now in order to avoid having to run the planner
+    // in a omp critial region later.
+    float complex *m_ell_m = fftwf_malloc(sizeof *m_ell_m * npol * nell * nm);
+    float *n_ell_phi = fftwf_malloc(sizeof *n_ell_phi * npol * nell * nphi);
+
+    plan_c2r = fftwf_plan_many_dft_c2r(1, nffts, npol * nell,
+				       m_ell_m, NULL,
+				       1, nm,
+				       n_ell_phi, NULL,
+				       1, nphi,
+				       FFTW_MEASURE);
+    fftwf_free(m_ell_m);
+    fftwf_free(n_ell_phi);
+
 
     #pragma omp parallel 
     {
         
-    fftwf_plan plan_c2r;
-    int nffts[1] = {nphi};
-
     float complex *m_ell_m = fftwf_malloc(sizeof *m_ell_m * npol * nell * nm);
     float *n_ell_phi = fftwf_malloc(sizeof *n_ell_phi * npol * nell * nphi);
     float *f_i_phi = fftwf_malloc(sizeof *f_i_phi * nufact * nphi);
@@ -143,14 +119,6 @@ float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_
 	fftwf_free(f_i_phi);
 	exit(1);
     }
-
-    #pragma omp critical
-    plan_c2r = fftwf_plan_many_dft_c2r(1, nffts, npol * nell,
-				       m_ell_m, NULL,
-				       1, nm,
-				       n_ell_phi, NULL,
-				       1, nphi,
-				       FFTW_ESTIMATE);
 
     #pragma omp for reduction (+:t_cubic)
     for (int tidx=0; tidx<ntheta; tidx++){
@@ -163,8 +131,6 @@ float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_
 	    * PI * ct_weights[tidx] / 3. / (float)nphi;
     }
 
-    fftwf_destroy_plan(plan_c2r);
-
     fftwf_free(m_ell_m);
     fftwf_free(n_ell_phi);
     fftwf_free(f_i_phi);
@@ -172,9 +138,9 @@ float t_cubic_sp(float *ct_weights, int *rule, float *f_i_ell, float complex *a_
     // End parallel region
     }
     
+    fftwf_destroy_plan(plan_c2r);
+
     return t_cubic;
 }
-
-// forward
 
 // step
