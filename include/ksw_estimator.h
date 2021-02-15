116c936aa0966853c7494b4373ceacf47f55e7fe
AdriJD <adri.j.duivenvoorden@gmail.com>
2021-02-15 00:52:11 -0500

    added forward and step to C code

--- include/ksw_estimator.h
+++ include/ksw_estimator.h
@@ -46,6 +46,36 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		 float *f_i_phi, int nell, int npol, int nufact, int nphi);
 
 /*
+ * Calculate the contribution of single ring to dT/dalm.
+ *
+ * Arguments
+ * ---------
+ * f_i_ell    : (nufact * npol * nell) array with unique factors.
+ * a_m_ell    : (npol * nell * nell) complex array with ell-major alms.
+ * y_m_ell    : (nell * nell) array with ell-major Ylms.
+ * m_ell_m    : (npol * nell * nm) complex array as input for ring fft, nm = nphi / 2 + 1.
+ * n_ell_phi  : (npol * nell * nphi) array as output for ring fft.
+ * plan_r2c   : fftw plan for ring rea2complex fft.
+ * f_i_phi    : (nufact * nphi) array for output unique factors on ring.
+ * work_i_ell : (nw, npol, nell) array for internal X_i_ell, Y_i_ell, Z_i_ell calculations.
+ * work_i_phi : (nw, nphi) array for internal dT/dX_i_phi, dT/dY_i_phi, dT/dZ_i_phi calulations.
+ * rule       : (nrule, 3) array of indices into f_i_phi that give X_i Y_i Z_i.
+ * weights    : (nrule, 3) array of weights for X_i Y_i Z_i.
+ * ct_weights : (ntheta) array of quadruture weights for cos(theta).
+ * nrule      : Number of rules.
+ * nw         : Number of elements in work arrays, see get_forward_array_size.
+ * nell       : Number of multipoles.
+ * npol       : Number of polarization dimensions.
+ * nphi       : Number of phi per ring.
+ */
+
+void forward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
+		float complex *m_ell_m, float *n_ell_phi, fftwf_plan plan_r2c,
+		float *f_i_phi, float *work_i_ell, float *work_i_phi,  int *rule,
+		float *weights, float ct_weight, int nrule, int nw, int nell, int npol,
+		int nphi);
+
+/*
  * Compute T[a] for a collection of rings.
  *
  * Arguments
@@ -56,8 +86,8 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
  * f_i_ell    : (nufact * npol * nell) array with unique factors.
  * a_m_ell    : (npol * nell * nell) complex array with ell-major alms.
  * y_m_ell    : (ntheta * nell * nell) array with ell-major Ylms for each ring.
- * ntheta     : number of thetas (rings).
- * nrule      : number of rules.
+ * ntheta     : Number of thetas (rings).
+ * nrule      : Number of rules.
  * nell       : Number of multipoles.
  * npol       : Number of polarization dimensions.
  * nufact     : Number of unique factors.
@@ -67,6 +97,30 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 float t_cubic_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell,
 		 float complex *a_m_ell, double *y_m_ell, int ntheta, int nrule,
 		 int nell, int npol, int nufact, int nphi);
+
+/*
+ * Compute forward and backward operation on collection of rings.
+ *
+ * Arguments
+ * ---------
+ * ct_weights : (ntheta) array of quadruture weights for cos(theta).
+ * rule       : (nrule, 3) array
+ * weights    : (nrule, 3) array of weights for X_i Y_i Z_i.
+ * f_i_ell    : (nufact * npol * nell) array with unique factors.
+ * a_m_ell    : (npol * nell * nell) complex array with ell-major alms.
+ * y_m_ell    : (ntheta * nell * nell) array with ell-major Ylms for each ring.
+ * grad_t     : (npol * nell * nell) complex array with ell-major alms.
+ * ntheta     : Number of thetas (rings).
+ * nrule      : Number of rules.
+ * nell       : Number of multipoles.
+ * npol       : Number of polarization dimensions.
+ * nufact     : Number of unique factors.
+ * nphi       : Number of phi per ring.
+ */
+
+void step_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell, 
+	     float complex *a_m_ell, double *y_m_ell, float complex *grad_t, 
+	     int ntheta, int nrule, int nell, int npol, int nufact, int nphi);
 
 /*
  * Compute the number of elements needed for the arrays in the forward function.
--- src/estimator.c
+++ src/estimator.c
@@ -1,11 +1,5 @@
 #include <ksw_estimator.h>
 
-// nufact = (ncomp * nr)
-// nrule = nfact
-
-// rule : (nfact, 3)
-// f_i_phi : (nufact, nphi)
-
 float t_cubic_on_ring_sp(int *rule, float *weights, float *f_i_phi, int nrule, int nphi){
 
     float t_cubic = 0.0;
@@ -20,9 +14,9 @@ float t_cubic_on_ring_sp(int *rule, float *weights, float *f_i_phi, int nrule, i
 	int wy = weights[ridx*3+1];
 	int wz = weights[ridx*3+2];
 
-	for (int pidx=0; pidx<nphi; pidx++){
-	    t_cubic += wx * wy * wz * f_i_phi[rx*nphi+pidx] * f_i_phi[ry*nphi+pidx]
-		       * f_i_phi[rz*nphi+pidx];
+	for (int phidx=0; phidx<nphi; phidx++){
+	    t_cubic += wx * wy * wz * f_i_phi[rx*nphi+phidx] * f_i_phi[ry*nphi+phidx]
+		       * f_i_phi[rz*nphi+phidx];
 	}
     }
     return t_cubic;
@@ -42,7 +36,7 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		complex double tmp;
 
 		if (midx < nell){
-		    tmp = (complex double) a_m_ell[pidx*nell*nm+lidx*nell+midx]
+		    tmp = (complex double) a_m_ell[pidx*nell*nell+lidx*nell+midx]
 			* (complex double) y_m_ell[lidx*nell+midx];
 		} else{
 		    // Needed because m_ell_m array can be larger than alm.
@@ -68,33 +62,14 @@ void backward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		0.0, f_i_phi, nphi);    
 }
 
-int get_forward_array_size(int *rule, int nrule){
-
-    int array_size = 0;
-
-    for (int ridx=0; ridx<nrule; ridx++){
-
-	int rx = rule[ridx*3];
-	int ry = rule[ridx*3+1];
-	int rz = rule[ridx*3+2];
-
-	array_size += 1;
-	
-	if (ry != rx){
-	    array_size += 1;
-	}
-	if (ry != rz && rx != rz){
-	    array_size += 1;
-	}
-    }
-    return array_size;
-}
-
 void forward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 		float complex *m_ell_m, float *n_ell_phi, fftwf_plan plan_r2c,
-		float *f_i_phi, float *x_i_ell, float *y_i_ell, float *z_i_ell,
-		float *dtdx_i_phi, float *dtdy_i_phi, float *dtdz_i_phi, int *rule,
-		float *weights, int nrule, int nell, int npol, int nufact, int nphi){
+		float *f_i_phi, float *work_i_ell, float *work_i_phi,  int *rule,
+		float *weights, float ct_weight, int nrule, int nw, int nell, int npol,
+		int nphi){
+
+    int widx = 0; // Index to work arrays.
+    int nm = nphi / 2 + 1;
 
     for (int ridx=0; ridx<nrule; ridx++){
 
@@ -102,11 +77,139 @@ void forward_sp(float *f_i_ell, float complex *a_m_ell, double *y_m_ell,
 	int ry = rule[ridx*3+1];
 	int rz = rule[ridx*3+2];
 
-	float wx = weights[ridx*3];
-	float wy = weights[ridx*3+1];
-	float wz = weights[ridx*3+2];	
+	float weight = weights[ridx*3] * weights[ridx*3+1] * weights[ridx*3+2]
+	    * PI * ct_weight / 3. / (float) nphi;
 
+	// Fill work arrays.
+	if (rx == ry && rx == rz){ // Case: 000.
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = 3. * weight
+			* f_i_ell[rx*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[widx*nphi+phidx] = f_i_phi[ry*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    widx += 1;
+	} else if (rx == ry && ry != rz){ // Case: 001.
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = 2. * weight
+			* f_i_ell[rx*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[(widx+1)*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[rz*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[widx*nphi+phidx] = f_i_phi[ry*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[(widx+1)*nphi+phidx] = f_i_phi[rx*nphi+phidx] * f_i_phi[ry*nphi+phidx];
+	    }
+	    widx += 2;
+	} else if (rx != ry && ry == rz){ // Case: 100.
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[rx*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[(widx+1)*npol*nell+pidx*nell+lidx] = 2. * weight
+			* f_i_ell[ry*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[widx*nphi+phidx] = f_i_phi[ry*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[(widx+1)*nphi+phidx] = f_i_phi[rx*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    widx += 2;
+	} else if (rx == rz && rx != ry){ // Case 010.
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = 2. * weight
+			* f_i_ell[rx*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[(widx+1)*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[ry*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[widx*nphi+phidx] = f_i_phi[ry*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[(widx+1)*nphi+phidx] = f_i_phi[rx*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    widx += 2;
+	} else { // Case: 012.
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[rx*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[widx*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[ry*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int pidx=0; pidx<npol; pidx++){
+		for (int lidx=0; lidx<nell; nell++){
+		    work_i_ell[(widx+1)*npol*nell+pidx*nell+lidx] = weight
+			* f_i_ell[rz*npol*nell+pidx*nell+lidx];
+		}
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[widx*nphi+phidx] = f_i_phi[ry*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[(widx+1)*nphi+phidx] = f_i_phi[rx*nphi+phidx] * f_i_phi[rz*nphi+phidx];
+	    }
+	    for (int phidx=0; phidx<nphi; phidx++){
+		work_i_phi[(widx+1)*nphi+phidx] = f_i_phi[rx*nphi+phidx] * f_i_phi[ry*nphi+phidx];
+	    }
+	    widx += 3;
+	}
     }
+
+    if (widx != (nw - 1)){
+	fprintf(stderr, "widx (%d) != nw - 1 (%d) \n", widx, nw);
+	exit(1);
+    }
+
+    // sum_i X_i_ell dT/dX_i_phi + Y_i_ell dT/dY_i_phi + Z_i_ell dT/dZ_i_phi -> n_ell_phi.
+    // Implemented as work_i_ell.T @ work_i_phi -> n_ell_phi.
+    cblas_sgemm(CblasRowMajor, CblasTrans, CblasNoTrans,
+		npol * nell, nphi, nw,
+		1.0, work_i_ell, npol * nell,
+		work_i_phi, nphi,
+		0.0, n_ell_phi, nphi);    
+
+    // r2c fft.
+    fftwf_execute_dft_r2c(plan_r2c, n_ell_phi, m_ell_m);
+
+    // Multiply by ylm and add result to alm.
+    for (int pidx=0; pidx<npol; pidx++){
+	for (int lidx=0; lidx<nell; lidx++){
+	    for (int midx=0; midx<nell; midx++){
+    
+		a_m_ell[pidx*nell*nell+lidx*nell+midx] += (complex float) 
+		    y_m_ell[lidx*nell+midx] * m_ell_m[pidx*nell*nm+lidx*nm+midx];
+	    }
+	}
+    }    
 }
 
 float t_cubic_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell, 
@@ -131,7 +234,6 @@ float t_cubic_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell,
 				       FFTW_MEASURE);
     fftwf_free(m_ell_m);
     fftwf_free(n_ell_phi);
-
 
     #pragma omp parallel 
     {
@@ -162,12 +264,117 @@ float t_cubic_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell,
     fftwf_free(n_ell_phi);
     fftwf_free(f_i_phi);
 
-    // End parallel region
-    }
+    } // End of parallel region
     
     fftwf_destroy_plan(plan_c2r);
 
     return t_cubic;
 }
 
-// step
+void step_sp(float *ct_weights, int *rule, float *weights, float *f_i_ell, 
+	     float complex *a_m_ell, double *y_m_ell, float complex *grad_t, 
+	     int ntheta, int nrule, int nell, int npol, int nufact, int nphi){
+ 
+    int nm = nphi / 2 + 1;
+    float t_cubic = 0.0;
+    int nffts[1] = {nphi};
+    fftwf_plan plan_c2r, plan_r2c;
+    int nw;
+
+    // Plan fft on temporary arrays now in order to avoid having to run the planner
+    // in a omp critial region later.
+    float complex *m_ell_m = fftwf_malloc(sizeof *m_ell_m * npol * nell * nm);
+    float *n_ell_phi = fftwf_malloc(sizeof *n_ell_phi * npol * nell * nphi);
+
+    plan_c2r = fftwf_plan_many_dft_c2r(1, nffts, npol * nell,
+				       m_ell_m, NULL,
+				       1, nm,
+				       n_ell_phi, NULL,
+				       1, nphi,
+				       FFTW_MEASURE);
+    plan_r2c = fftwf_plan_many_dft_r2c(1, nffts, npol * nell,
+				       n_ell_phi, NULL,
+				       1, nphi,
+				       m_ell_m, NULL,
+				       1, nm,
+				       FFTW_MEASURE);
+    
+    fftwf_free(m_ell_m);
+    fftwf_free(n_ell_phi);
+
+    nw = get_forward_array_size(rule, nrule);
+
+    #pragma omp parallel 
+    {
+        
+    float complex *m_ell_m = fftwf_malloc(sizeof *m_ell_m * npol * nell * nm);
+    float *n_ell_phi = fftwf_malloc(sizeof *n_ell_phi * npol * nell * nphi);
+    float *f_i_phi = fftwf_malloc(sizeof *f_i_phi * nufact * nphi);
+    float *work_i_ell = fftwf_malloc(sizeof *work_i_ell * nw * npol * nell);    
+    float *work_i_phi = fftwf_malloc(sizeof *work_i_phi * nw * nphi);    
+    float complex *grad_t_priv = fftwf_malloc(sizeof *grad_t_priv * npol * nell * nell);	
+
+    if (m_ell_m == NULL || n_ell_phi == NULL || f_i_phi == NULL){
+	fftwf_free(m_ell_m);
+	fftwf_free(n_ell_phi);
+	fftwf_free(f_i_phi);
+	exit(1);
+    }
+
+    #pragma omp for
+    for (int tidx=0; tidx<ntheta; tidx++){
+
+	backward_sp(f_i_ell, a_m_ell, y_m_ell + tidx * nell * nell,
+		    m_ell_m, n_ell_phi, plan_c2r,
+		    f_i_phi, nell, npol, nufact, nphi);
+
+	forward_sp(f_i_ell, grad_t_priv, y_m_ell, m_ell_m, n_ell_phi, plan_r2c,
+		   f_i_phi, work_i_ell, work_i_phi, rule, weights, 
+		   ct_weights[tidx], nrule, nw, nell, npol, nphi);		
+    }
+
+    #pragma omp critical
+    {
+	for (int i=0; i<npol*nell*nell; i++){
+	    grad_t[i] += grad_t_priv[i];
+	}	
+    }
+
+    fftwf_free(m_ell_m);
+    fftwf_free(n_ell_phi);
+    fftwf_free(f_i_phi);
+    fftwf_free(work_i_ell);
+    fftwf_free(work_i_phi);
+    fftwf_free(grad_t_priv);
+
+    } // End of parallel region
+    
+    fftwf_destroy_plan(plan_c2r);
+    fftwf_destroy_plan(plan_r2c);
+}
+
+int get_forward_array_size(int *rule, int nrule){
+
+    int array_size = 0;
+
+    for (int ridx=0; ridx<nrule; ridx++){
+
+	int rx = rule[ridx*3];
+	int ry = rule[ridx*3+1];
+	int rz = rule[ridx*3+2];
+
+	array_size += 1;
+	
+	if (ry != rx){
+	    array_size += 1;
+	}
+	if (ry != rz && rx != rz){
+	    array_size += 1;
+	}
+    }
+    return array_size;
+}
+
+void compute_ylm(){
+
+}
